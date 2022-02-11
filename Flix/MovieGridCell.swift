fb4ed64a46ac5aaabf85433914d3fb0ed4a5e345
Jfran258 <Jfran258@fiu.edu>
2022-02-10 21:35:57 -0500

    new
    commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jason Francis on 2/10/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
